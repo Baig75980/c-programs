1c508d36ef8b96e3608bb6ff19084c56c22596a9
Baig75980 <baig75980@gmail.com>
2023-05-22 20:24:29 +0530

    new

---
+++ basic program/loops/l4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("\nEnter the number=");
+    scanf("%d",&n);
+    for (int i =1; i <=10; i=i+1)
+    {
+        printf("%d*%d=%d\n",n,i,n*i);
+    }
+    return 0;
+}
